ec77b4101d06c1103126460bec01d38752862cc4
Eric Vicenti <evv@fb.com>
2016-11-28 08:55:18 -0800

    yes

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log('YES, RUNNING FROM VVV!')
+console.log('YES, RUNNING FROM VVV!', JSON.stringify(process.env))
+
+var keygen = require('ssh-keygen');
+
+
+
+if (!process.env.GH_DEPLOYMENT_KEY) {
+  console.log('You need to set up your github deployment key!');
+  keygen({
+    comment: 'ci-deploy-key',
+  }, function(err, out){
+    if(err) {
+      console.log('Something went wrong: '+err);
+      return;
+    }
+    console.log('Keys created!');
+    console.log('private key: '+out.key);
+    console.log('public key: '+out.pubKey);
+  });
+}
