2e4a2555d146249244cd72cad603178b9e0daf29
Eric Vicenti <evv@fb.com>
2016-11-28 09:29:55 -0800

    yes

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 
 var keygen = require('ssh-keygen');
+var fs = require('fs-extra');
+var exec = require('child_process').execFileSync();
+var join = require('path').join;
 
 
 var generalMessage = ''+
@@ -9,7 +12,7 @@ var generalMessage = ''+
 var ghOrg = process.env.CIRCLE_PROJECT_USERNAME;
 var ghRepo = process.env.CIRCLE_PROJECT_REPONAME;
 var ghLink = 'https://github.com/'+ghOrg+'/'+ghRepo+'/settings/keys';
-var circleLink = 'https://circleci.com/gh/'+ghOrg+'/'+ghRepo+'/edit';
+var circleLink = 'https://circleci.com/gh/'+ghOrg+'/'+ghRepo+'/edit#env-vars';
 
 if (!process.env.GH_DEPLOYMENT_KEY) {
   keygen({
@@ -24,9 +27,9 @@ if (!process.env.GH_DEPLOYMENT_KEY) {
     var priv = (new Buffer(out.key)).toString('hex');
     console.log(generalMessage);
     console.log('');
-    console.log('1. Go to '+ghLink)
+    console.log('1. Go to '+ghLink);
     console.log('');
-    console.log('Add this public key: ')
+    console.log('Add this public key, and check the box to allow write access. You may call it "Circle-GHPages Deploy Key":');
     console.log(out.pubKey);
     console.log('2. Go to '+circleLink);
     console.log('');
@@ -37,4 +40,7 @@ if (!process.env.GH_DEPLOYMENT_KEY) {
   });
 } else {
   console.log('rock and roll!', process.env.GH_DEPLOYMENT_KEY);
+  var keyPath = join(__dirname, 'access.key');
+  fs.writeFileSync(keyPath, new Buffer(process.env.GH_DEPLOYMENT_KEY, 'hex'));
+  console.log('git clone', keyPath);
 }
